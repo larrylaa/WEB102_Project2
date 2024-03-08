767bd079050832aff3db35984fbf51c77e7a43e3
Larry La <larryquocla@gmail.com>
2024-03-07 23:38:53 -0800

    feat: initial styling

--- Calculus_Flashcards/src/App.jsx
+++ Calculus_Flashcards/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Trigonometry Essentials For Calculus!</h1>
+      <p>How strong are your trigonometry fundamentals? Test or practice your knowledge here!</p>
+      <h4>Cards Reviewed: 1</h4>
+    </div>
+
+    <div className="centercard">
+      <Card></Card>
+    </div>
+
+
+    <div className="centerbutton">
+      <button>Back</button>
+      <button>Next</button>
+    </div>
+
     </>
   )
 }
---
+++ Calculus_Flashcards/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './card.css'
+
+const Card = () => {
+    return(
+        <div className="flashcard">
+            <h1>Card Here</h1>
+        </div>
+
+    );
+}
+
+export default Card;
